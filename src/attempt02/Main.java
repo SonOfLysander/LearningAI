d849721feb90ee1f9a6139490fbd453f744f0471
Paul Baker Le Salt Shaker <son.of.lysander@gmail.com>
2014-01-20 12:37:34 -0700

    Some small progress. Updating.

--- src/attempt02/Main.java
+++ src/attempt02/Main.java
@@ -1,14 +1,24 @@
 package attempt02;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
 
 	// this must be an even number, for now
-	private int populationSize = 100;
+	private static int populationSize = 100;
 	// The maximum number of generations before giving up.
 	// 400 was used in original.
-	private int maxGenerationsBeforeAppocolypse = Integer.MAX_VALUE;
+	private static int maxGenerationsBeforeAppocolypse = Integer.MAX_VALUE;
 
 	public static void main(String[] args) {
-
+		Random rand = new Random();
+		int target = 42;
+		List<Member> poolA = new ArrayList<Member>(populationSize); // starting pool
+		List<Member> poolB = new ArrayList<Member>(populationSize); // buffer pool
+		for (int i = 0; i < poolA.size(); i++){
+			poolA.add(new Member(target));
+		}
 	}
 }
--- src/attempt02/Member.java
+++ src/attempt02/Member.java
@@ -6,13 +6,28 @@ public class Member {
 	private static Random rand = new Random();
 	private static float crossoverRate = 0.7f;
 	private static float mutationRate = 0.001f;
-	private static int chromosomeLength;
+	private static int chromosomeLength = 300;
+	private static char[] genes = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '+', '*', '/' };
 
+	private int target;
 	private String chromosome;
 	private float fitness;
 
 	public Member(int target) {
+		this(target, randomChromosome());
 	}
-	public Member(int target, String chromosome){
+
+	public Member(int target, String chromosome) {
+		this.target = target;
+		this.chromosome = chromosome;
+		this.fitness = 0;
+	}
+
+	private static String randomChromosome() {
+		StringBuilder result = new StringBuilder();
+		for (int i = 0; i < chromosomeLength; i++){
+			result.append(genes[rand.nextInt(genes.length)]);
+		}
+		return result.toString();
 	}
 }
