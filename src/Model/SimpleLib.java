2c65912f27abe12b0091dd5c7c497b700ac77615
Paul Baker <son.of.lysander@gmail.com>
2014-05-13 02:59:02 -0600

    Significant progress. Much cleaner than I remember doing it last time.

--- src/Model/SimpleLib.java
+++ src/Model/SimpleLib.java
@@ -4,12 +4,20 @@ import java.util.Random;
 
 public class SimpleLib {
 	public static class NotImplementedException extends RuntimeException {
-	    private static final long serialVersionUID = 1L;
-	    public NotImplementedException(){}
+		private static final long serialVersionUID = 1L;
+
+		public NotImplementedException() {
+		}
 	}
 
 	private static Random rand = new Random();
-	public static int randMinMax(int min, int max) throws Exception{
-		throw new NotImplementedException();
+
+	public static int randMinMax(int min, int max) {
+		if (max < min) {
+			int swp = min;
+			min = max;
+			max = swp;
+		}
+		return rand.nextInt(max - min) + min;
 	}
 }
