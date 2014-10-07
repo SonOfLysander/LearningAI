eda88a32c9a573df6269a7dc524049db8010b2fc
Paul Baker <paul.n.baker@utah.edu>
2014-10-07 17:27:44 -0600

    Added thourough test for getGenes. Probably not really all that necessary, but it's important to verify that the getter that will be used for other tests is functioning

--- test/geneticAlgorithm/ChromosomeTest.java
+++ test/geneticAlgorithm/ChromosomeTest.java
@@ -2,11 +2,15 @@ package geneticAlgorithm;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class ChromosomeTest {
+
+	private static final Random rand = new Random();
 
 	@Before
 	public void setUp() throws Exception {
@@ -18,7 +22,14 @@ public class ChromosomeTest {
 
 	@Test
 	public void testGetGenes() {
-		fail("Not yet implemented");
+		assertEquals(1, new Chromosome(1).getGenes());
+		assertEquals(0, new Chromosome(0).getGenes());
+		assertEquals(Long.MAX_VALUE, new Chromosome(Long.MAX_VALUE).getGenes());
+		assertEquals(Long.MIN_VALUE, new Chromosome(Long.MIN_VALUE).getGenes());
+		for (int i = 0; i < 25000; i++) {
+			long l = rand.nextLong();
+			assertEquals(l, new Chromosome(l).getGenes());
+		}
 	}
 
 	@Test
