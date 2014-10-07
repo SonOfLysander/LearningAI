f7ed3a886c51cea670437b7285339a476316005b
Paul Baker <paul.n.baker@utah.edu>
2014-10-07 17:01:01 -0600

    Added method to create a new chromosome by crossing two chromosomes over each other. Using two utility methods from stack overflow. I can remove them and do this better with bit manipulations

--- src/geneticAlgorithm/Chromosome.java
+++ src/geneticAlgorithm/Chromosome.java
@@ -1,12 +1,13 @@
 package geneticAlgorithm;
 
+import java.nio.ByteBuffer;
 import java.util.Random;
 
 public class Chromosome {
 
-	private static Random rand = new Random();
-	private static float crossoverRate = 0.7f;
-	private static double mutationRate = 0.001f;
+	private static final Random rand = new Random();
+	private static final float crossoverRate = 0.7f;
+	private static final double mutationRate = 0.001f;
 
 	private long genes;
 
@@ -22,8 +23,50 @@ public class Chromosome {
 		return genes;
 	}
 
+	public Chromosome crossover(Chromosome other) {
+		byte[] geneArray = longToBytes(getGenes());
+		byte[] otherArray = longToBytes(other.getGenes());
+		int crossoverIndex = rand.nextInt(geneArray.length);
+		byte[] crossedGenes = new byte[geneArray.length];
+		for (int i = 0; i < crossedGenes.length; i++) {
+			crossedGenes[i] = i < crossoverIndex ? geneArray[i] : otherArray[i];
+		}
+		return new Chromosome(bytesToLong(crossedGenes));
+	}
+	
 	@Override
 	public String toString() {
 		return Long.toBinaryString(genes);
+	}
+
+	/**
+	 * Taken from StackOverflow. Simple utility method to convert a long into a
+	 * byte[].
+	 * 
+	 * @author http://stackoverflow.com/a/4485196/1478636
+	 * 
+	 * @param x
+	 * @return
+	 */
+	private byte[] longToBytes(long x) {
+		ByteBuffer buffer = ByteBuffer.allocate(Long.SIZE);
+		buffer.putLong(x);
+		return buffer.array();
+	}
+
+	/**
+	 * Taken from StackOverflow. Simple utility method to convert a byte[] into
+	 * a long
+	 * 
+	 * @author http://stackoverflow.com/a/4485196/1478636
+	 * 
+	 * @param bytes
+	 * @return
+	 */
+	private long bytesToLong(byte[] bytes) {
+		ByteBuffer buffer = ByteBuffer.allocate(Long.SIZE);
+		buffer.put(bytes);
+		buffer.flip();// need flip
+		return buffer.getLong();
 	}
 }
