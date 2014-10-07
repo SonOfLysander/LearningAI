196dc0087c0c6e0236aafde3b4171cc739ef430b
Paul Baker <paul.n.baker@utah.edu>
2014-10-07 17:21:38 -0600

    Stock JUnit4 test for Chromosome

---
+++ test/geneticAlgorithm/ChromosomeTest.java
@@ -0,0 +1,29 @@
+package geneticAlgorithm;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ChromosomeTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetGenes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCrossover() {
+		fail("Not yet implemented");
+	}
+
+}
