bc46658ffd489adba3b7cd897d654df295ef002a
Paul Baker <=>
2015-02-19 12:18:48 -0700

    Rewrote more code. Much cleaner, again

--- src/io/paulbaker/ai/simple_genetic_algorithm/Application.java
+++ src/io/paulbaker/ai/simple_genetic_algorithm/Application.java
@@ -4,4 +4,9 @@ package io.paulbaker.ai.simple_genetic_algorithm;
  * Created by paulbaker on 2/19/15.
  */
 public class Application {
+  public static void main(String[] args) {
+    System.out.println(Long.SIZE);
+    System.out.println(Long.SIZE / 4);
+
+  }
 }
--- src/io/paulbaker/ai/simple_genetic_algorithm/PopulationMember.java
+++ src/io/paulbaker/ai/simple_genetic_algorithm/PopulationMember.java
@@ -1,7 +1,70 @@
 package io.paulbaker.ai.simple_genetic_algorithm;
 
+import java.util.Random;
+
 /**
  * Created by paulbaker on 2/19/15.
  */
 public class PopulationMember {
+
+  private static final Random RANDOM;
+
+  static {
+    synchronized (PopulationMember.class) {
+      RANDOM = new Random();
+    }
+  }
+
+  private long chromosome;
+
+  private Integer parsedValue;
+
+  public PopulationMember() {
+    this(RANDOM.nextLong());
+  }
+
+  public PopulationMember(long chromosome) {
+    this.chromosome = chromosome;
+    parsedValue = null;
+  }
+
+  public int parse() {
+    if (parsedValue == null) {
+      parsedValue = 0;
+      int currentOperator = 10;
+      boolean onNumberNotOperator = true;
+      for (int i = 0; i < Long.SIZE / 4; i++) {
+        // Trivia note to self/others, a "NIBBLE" is the term for four bits.
+        int nibble = (int) ((chromosome >> (((Long.SIZE / 4) - i + 1) * 4)) & 0xF);
+        if (onNumberNotOperator && nibble >= 0 && nibble < 10) {
+          switch (currentOperator) {
+            case 10:
+              parsedValue += nibble;
+              break;
+            case 11:
+              parsedValue -= nibble;
+              break;
+            case 12:
+              parsedValue *= nibble;
+              break;
+            case 13:
+              // If the nibble is 0, we skip it. We can't divide by
+              // zero and we don't flip our searching boolean because
+              // we continue until we find a valid option
+              if (nibble == 0)
+                continue;
+              parsedValue /= nibble;
+              break;
+          }
+          onNumberNotOperator = !onNumberNotOperator;
+        }
+        else if (onNumberNotOperator == false && nibble >= 10 && nibble < 14) {
+          currentOperator = nibble;
+          onNumberNotOperator = !onNumberNotOperator;
+        }
+      }
+    }
+    return parsedValue;
+  }
+
 }
