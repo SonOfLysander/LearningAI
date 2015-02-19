70f4de3d809c700325462e15a17d633ebeb050fd
Paul Baker <=>
2015-02-19 11:40:40 -0700

    Initial setup, already much cleaner

--- src/io/paulbaker/ai/simple_genetic_algorithm/Population.java
+++ src/io/paulbaker/ai/simple_genetic_algorithm/Population.java
@@ -1,7 +1,32 @@
 package io.paulbaker.ai.simple_genetic_algorithm;
 
+import java.util.Iterator;
+import java.util.Random;
+
 /**
  * Created by paulbaker on 2/19/15.
  */
 public class Population {
+
+  private static final Random RANDOM;
+
+  static {
+    synchronized (Population.class) {
+      RANDOM = new Random();
+    }
+  }
+
+  private final double mutationRate;
+
+  private final double crossOverRate;
+
+  private PopulationMember[] populationMembers;
+
+  public Population(int populationSize, double mutationRate, double crossOverRate) {
+    populationMembers = new PopulationMember[populationSize];
+    this.mutationRate = mutationRate;
+    this.crossOverRate = crossOverRate;
+
+  }
+
 }
