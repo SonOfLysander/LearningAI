cc09cb8260c9b73951f94cc15ef5ed75b3bf4832
Paul Baker <paul.n.baker@utah.edu>
2015-11-04 02:20:56 -0700

    Changed test to generate Strings.

--- src/test/java/io/paulbaker/ai/geneticalgorithm/ChromosomeTest.java
+++ src/test/java/io/paulbaker/ai/geneticalgorithm/ChromosomeTest.java
@@ -8,6 +8,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.Random;
+import java.util.function.Supplier;
 
 import static org.testng.AssertJUnit.assertEquals;
 
@@ -23,24 +24,39 @@ public class ChromosomeTest extends AbstractTestNGSpringContextTests {
   @Autowired
   private Random random;
 
-  @DataProvider(name = "geneStrings")
-  public Object[][] generateStrings() {
-    Object[][] objects = new Object[100][];
-    for (int i = 0; i < objects.length; i++) {
-      StringBuilder stringBuilder = new StringBuilder();
-      for (int j = 0; j < Long.SIZE; j++) {
-        stringBuilder.append(random.nextBoolean() ? "1" : "0");
+  private Object[][] generateStrings(int count, int length, Supplier<Character> lambda) {
+    Object[][] strings = new Object[count][];
+    for (int i = 0; i < strings.length; i++) {
+      StringBuilder sb = new StringBuilder();
+      for (int j = 0; j < length; j++) {
+        sb.append(lambda.get());
       }
-
-      objects[i] = new Object[]{stringBuilder.toString()};
+      strings[i] = new Object[]{sb.toString()};
     }
-    return objects;
+    return strings;
+  }
+
+  @DataProvider(name = "geneStrings")
+  public Object[][] geneStrings() {
+    // Generates a proper string of 0s and 1s.
+    return generateStrings(100, Long.SIZE, () -> random.nextBoolean() ? '1' : '0');
   }
 
   @Test(dataProvider = "geneStrings")
   public void testConstructedFromString(String string) {
     Chromosome chromosome = Chromosome.fromString(string);
     assertEquals("Chromosome does not parse from string correctly", string, chromosome.toString());
+  }
+
+  @DataProvider(name = "malformedStrings")
+  public Object[][] malformedStrings() {
+    // Generates strings of correct length, but using digits [2,9].
+    return generateStrings(100, Long.SIZE, () -> (char) (random.nextInt(8) + 50));
+  }
+
+  @Test(dataProvider = "malformedStrings", expectedExceptions = IllegalArgumentException.class)
+  public void testMalformedStrings(String string) {
+    Chromosome.fromString(string);
   }
 
 }
