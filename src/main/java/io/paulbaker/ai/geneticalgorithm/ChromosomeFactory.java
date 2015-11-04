62b7b116c7386174c95a1efe9f50755296b08f30
Paul Baker <paul.n.baker@utah.edu>
2015-11-04 03:41:09 -0700

    Adding a breed-chromosome method.

--- src/main/java/io/paulbaker/ai/geneticalgorithm/ChromosomeFactory.java
+++ src/main/java/io/paulbaker/ai/geneticalgorithm/ChromosomeFactory.java
@@ -14,10 +14,21 @@ public class ChromosomeFactory {
   @Autowired
   private Random random;
 
-  public Chromosome generateNewGene(){
+  public Chromosome generateNewGene() {
     return Chromosome.fromLong(random.nextLong());
   }
 
+  public Chromosome[] breedChromosomes(Chromosome a, Chromosome b) {
+    String aString = a.toString();
+    String bString = b.toString();
+    int index = random.nextInt(aString.length() - 2) + 1;
 
+    String oneString = aString.substring(0, index) + bString.substring(index);
+    String twoString = bString.substring(0, index) + aString.substring(index);
+
+    return new Chromosome[]{
+      Chromosome.fromString(oneString), Chromosome.fromString(twoString)
+    };
+  }
 
 }
