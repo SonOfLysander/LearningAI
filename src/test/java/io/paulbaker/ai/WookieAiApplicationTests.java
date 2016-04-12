c61a184c9f36eb2a0458fb7075b8e7c5083a3d58
Paul Baker <paul.n.baker@utah.edu>
2016-04-12 02:26:35 -0600

    Initial commit.

---
+++ src/test/java/io/paulbaker/ai/WookieAiApplicationTests.java
@@ -0,0 +1,16 @@
+package io.paulbaker.ai;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = WookieAiApplication.class)
+public class WookieAiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
